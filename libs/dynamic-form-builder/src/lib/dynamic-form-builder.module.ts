59be90c3595ad23f370196cfe2232132d85a6079
ruslan gutu <gutur@icon-intl.com>
2019-04-01 08:45:58 -0400

    npm pack

--- libs/dynamic-form-builder/src/lib/dynamic-form-builder.module.ts
+++ libs/dynamic-form-builder/src/lib/dynamic-form-builder.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DynamicFormBuilderService } from './dynamic-form-builder-service';
 
-@NgModule({
-  declarations: [DynamicFormBuilderService],
-  imports: [CommonModule],
-  exports: [DynamicFormBuilderService]
+@NgModule({  
+  imports: [CommonModule]
 })
 export class DynamicFormBuilderModule {}
