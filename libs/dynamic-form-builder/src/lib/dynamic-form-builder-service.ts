26865c6d97fc690990c6905c632f68ae72a6a8dd
admin <ruslan_gutu@yahoo.com>
2019-04-13 21:11:51 -0400

    fix for null

--- libs/dynamic-form-builder/src/lib/dynamic-form-builder-service.ts
+++ libs/dynamic-form-builder/src/lib/dynamic-form-builder-service.ts
@@ -13,27 +13,26 @@ export class DynamicFormBuilderService{
     isObjectArray = val => Array.isArray(val) && !this.isString(val)
 
     ccFormBuilder = (obj = {}, group : any = this.fb.group({})) => Object.entries(obj)
-        .reduce(
-            (result, [key,val]) => {
-                //debugger;
-                if(this.isObject(val)){
-                    const newGroup = this.ccFormBuilder(val || '', this.fb.group({}))
-                    this.isObjectArray(result)
-                        ? result.push(newGroup)
-                        : result.controls[key] = newGroup;
-                }
-                else if (Array.isArray(val)){
-                    const newArray = Object.values(this.ccFormBuilder(val, []))
-                    result.addControl(key, this.fb.array(newArray))
-                    
-                }
-                else{                
-                    result.addControl(key, new FormControl(val || ''))
-                }
-                return result;
+    .reduce(
+        (result, [key,val]) => {
+            if(val && this.isObject(val)){
+                const newGroup = this.ccFormBuilder(val, this.fb.group({}))
+                this.isObjectArray(result)
+                    ? result.push(newGroup)
+                    : result.controls[key] = newGroup;
             }
-            , group
-        )           
+            else if (val && Array.isArray(val)){
+                const newArray = Object.values(this.ccFormBuilder(val, []))
+                result.addControl(key, this.fb.array(newArray))
+                
+            }
+            else{                
+                result.addControl(key, new FormControl(val))
+            }
+            return result;
+        }
+        , group
+    )                    
 
     get formBuilder () : FormBuilder {
         return this.fb
